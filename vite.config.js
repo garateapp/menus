dd40b8c9c6f1fd006aec857b26b0b053b7fd7c3c
calgreenex <carlos.alvarez@greenex.cl>
2026-03-30 12:00:52 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: 'resources/js/app.tsx',
             refresh: true,
         }),
         react(),
     ],
-
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
+        },
+    },
 });
